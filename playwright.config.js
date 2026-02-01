5326a19b4258d094e8fa2050ddcda5f0c51dd264
Jenny <lisalthe@gmail.com>
2026-02-01 21:55:33 +0300

    fix: update root playwright.config.js webServer to use npx @hexlet/chat-server

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run start --prefix frontend',
+    command: 'npx @hexlet/chat-server -s ./frontend/dist',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
