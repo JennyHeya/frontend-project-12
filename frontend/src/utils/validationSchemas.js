bb60a0609f1a71e2263657ee8fda15b43ad09a65
Jenny <lisalthe@gmail.com>
2026-02-19 01:52:11 +0300

    fix validations

--- frontend/src/utils/validationSchemas.js
+++ frontend/src/utils/validationSchemas.js
@@ -17,16 +17,20 @@ export const signupSchema = yup.object({
       .oneOf([yup.ref('password')], t('validation.signup.notConfirmPassword')),
   })
 
-export const loginSchema = yup.object().shape({
-  username: yup.string().required('validation.required'),
-  password: yup.string().required('validation.required'),
-})
+export const loginSchema = yup.object({
+    username: yup.string().required(t('validation.required')),
+    password: yup.string().required(t('validation.required')),
+  })
 
-export const channelNameSchema = yup.object().shape({
-  name: yup
-    .string()
-    .trim()
-    .min(3, 'validation.channel.nameLength')
-    .max(20, 'validation.channel.nameLength')
-    .required('validation.required'),
-})
+export const channelNameSchema = yup.object({
+    name: yup
+      .string()
+      .min(3, t('validation.channelsValidation.length'))
+      .max(20, t('validation.channelsValidation.length'))
+      .required(t('validation.required'))
+      .test(
+        'is name uniq',
+        t('validation.channelsValidation.duplicate'),
+        val => !channels.some(({ name }) => name === val),
+      ),
+  })
