896933a2f6f063c3b078ab811ec6eacca8675e0c
Jenny <lisalthe@gmail.com>
2026-02-19 01:43:19 +0300

    refactor: extract all validation schemas to utils/validationSchemas.js

--- frontend/src/utils/validationSchemas.js
+++ frontend/src/utils/validationSchemas.js
@@ -1,21 +1,21 @@
 import * as yup from 'yup'
 
-export const signupSchema = yup.object().shape({
-  username: yup
-    .string()
-    .min(3, 'validation.signup.nameLength')
-    .max(20, 'validation.signup.nameLength')
-    .required('validation.required'),
-  password: yup
-    .string()
-    .min(6, 'validation.signup.passwordLength')
-    .required('validation.required'),
-  confirmPassword: yup
-    .string()
-    .required('validation.required')
-    .min(6, 'validation.signup.passwordLength')
-    .oneOf([yup.ref('password')], 'validation.signup.notConfirmPassword'),
-})
+export const signupSchema = yup.object({
+    username: yup
+      .string()
+      .min(3, t('validation.signup.nameLength'))
+      .max(20, t('validation.signup.nameLength'))
+      .required(t('validation.required')),
+    password: yup
+      .string()
+      .min(6, t('validation.signup.passwordLength'))
+      .required(t('validation.required')),
+    confirmPassword: yup
+      .string()
+      .required(t('validation.required'))
+      .min(6, t('validation.signup.passwordLength'))
+      .oneOf([yup.ref('password')], t('validation.signup.notConfirmPassword')),
+  })
 
 export const loginSchema = yup.object().shape({
   username: yup.string().required('validation.required'),
